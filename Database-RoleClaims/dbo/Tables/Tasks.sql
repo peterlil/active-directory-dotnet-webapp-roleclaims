241797e989d1978c1a8fd9e160c39f62d67bdd3c
Peter Liljenroth <peterlil@microsoft.com>
2017-08-23 08:24:41 +0000

    Added the database project and imported the Tasks table.

---
+++ Database-RoleClaims/dbo/Tables/Tasks.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tasks] (
+    [TaskID]   INT            IDENTITY (1, 1) NOT NULL,
+    [TaskText] NVARCHAR (MAX) NULL,
+    [Status]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Tasks] PRIMARY KEY CLUSTERED ([TaskID] ASC)
+);
+
