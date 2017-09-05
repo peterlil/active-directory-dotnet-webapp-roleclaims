62ef53cb9b8d668bfb783c83ac49e0ba0c693913
Peter Liljenroth <peterlil@microsoft.com>
2017-09-05 15:15:09 +0200

    Misc

--- Config.ps1
+++ Config.ps1
@@ -1,5 +1,5 @@
    # Variables for the registration of the AAD application
-    $appName = "TaskTrackerWebApp"
+    $appName = "TaskTrackerWebApp52"
     $homePage = "https://localhost:44322"
     $appIdURI = "https://$tenantName/$appName"
     $logoutURI = "$homePage/Account/EndSession"
--- WebApp-RoleClaims-DotNet/App_Start/Startup.Auth.cs
+++ WebApp-RoleClaims-DotNet/App_Start/Startup.Auth.cs
@@ -1,4 +1,4 @@
-﻿// #define SingleTenantApp
+#define SingleTenantApp
 using System;
 
 //The following namespace was added to this sample.
